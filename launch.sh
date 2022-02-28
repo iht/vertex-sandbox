c241346efd399c2f8e9bfe33d9a567c34084e49b
Israel Herraiz <ihr@google.com>
2022-02-28 10:12:53 +0100

    Script for launching the pipeline

---
+++ launch.sh
@@ -0,0 +1,7 @@
+python pipeline.py --query="" \
+  --pipeline-name="ihr-my-pipeline" \
+  --pipeline-root="gs://ihr-live-workshop/pipeline/" \
+  --project-id="ihr-vertex-pipelines" \
+  --temp-location="gs://ihr-live-workshop/tmp/" \
+  --region="europe-west4" \
+  --service-account="ihr-vertex-live-workshop@ihr-vertex-pipelines.iam.gserviceaccount.com"

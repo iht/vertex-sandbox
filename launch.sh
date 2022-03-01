b78f1394ef4ae3b75a6f25f3ef0d61f318e6f6a4
Israel Herraiz <ihr@google.com>
2022-03-01 10:10:19 +0100

    Copy file always before running

--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
+gsutil cp transform.py gs://ihr-live-workshop/pipeline/transform.py
+
 python pipeline.py --query="SELECT * FROM ihr_live_workshop.transactions" \
   --pipeline-name="ihr-my-pipeline" \
   --pipeline-root="gs://ihr-live-workshop/pipeline/" \
